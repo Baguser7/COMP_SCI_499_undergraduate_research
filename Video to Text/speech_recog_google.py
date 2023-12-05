3d8eff971ecfee9f25c15c56eeb97a0cefeadb1d
Baguser7 <bagushendrawan01@gmail.com>
2023-12-05 15:39:15 -0600

    Speech to text

--- Video to Text/speech_recog_google.py
+++ Video to Text/speech_recog_google.py
@@ -5,9 +5,14 @@
 
 import moviepy.editor as mp 
 import speech_recognition as sr 
+import get_data
   
 # Load the video 
-video = mp.VideoFileClip(r"Video to Text\jp.mp4") 
+#video = mp.VideoFileClip(r"COMP_SCI_499_undergraduate_research\Video to Text\Video\2_9gag.mp4")
+url = 'https://vm.tiktok.com/ZT85rcSLS/'
+local = 'toxic1.mp4'
+get_data.download_file(url,local)
+video = mp.VideoFileClip("toxic1.mp4")
   
 # Extract the audio from the video 
 audio_file = video.audio 
@@ -21,7 +26,7 @@ with sr.AudioFile("1.wav") as source:
     data = r.record(source) 
   
 # Convert speech to text 
-text = r.recognize_ibm(data)
+text = r.recognize_houndify(data, client_id='74f9EOm76HkxNQ7QZJeZLA==', client_key='zaejC92luOFmDXiTyV9wKfPh89HTajfRNFZaP7JUEOrA3oF3YDiX0J8Lc2ZmbZPxl7rj0cMu28J-4e75lT2UpQ==')
   
 # Print the text 
 print("\nThe resultant text from video is: \n") 
